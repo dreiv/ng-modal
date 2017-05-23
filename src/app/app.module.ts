59e99da504c0e8803a3d9f4fa6d23a1bd211f3dc
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2017-05-23 18:14:44 +0300

    Multi-Slot Transclusion :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -17,6 +17,7 @@ import { ModalComponent } from './modal/modal.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppModule { }
